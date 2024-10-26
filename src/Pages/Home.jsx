389b1b004978f602fb17c6cc3e0c09ceccd6f94c
jayshreee10 <2001109280_ee@pmec.ac.in>
2024-10-26 21:11:47 +0530

    fix:name

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,11 +6,11 @@ function Home() {
   const navigate = useNavigate();
 
   return (
-    <div className="relative flex flex-col items-center justify-center w-full h-screen overflow-hidden bg-black md:shadow-xl">
+    <div className="relative mt-[70px] flex flex-col items-center justify-center w-full h-screen overflow-hidden bg-black md:shadow-xl">
       <div className="absolute inset-0 dark:from-white dark:to-slate-900/10"></div>
 
       <div className="relative z-10 flex flex-col items-center justify-center w-full text-center">
-        <div className="md:text-7xl  lg:text-[15vw] sm:text-6xl max-sm:text-4xl max-sm:w-full text-white font-Lobster font-semibold tracking-wider">
+        <div className="md:text-7xl  lg:text-[13vw] sm:text-6xl max-sm:text-4xl max-sm:w-full text-white font-Lobster font-semibold tracking-wider">
           Jayshree <br /> <span className="ml-[200px]">Sadangi</span>
           <p
             className="flex items-center justify-end text-LayoutPink tracking-wide my-28 text-xl font-semibold cursor-pointer font-Alata"
